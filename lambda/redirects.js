89c8521709f9173794b57de725aef7a0e77a66c1
Lucas Coelho <lucascoelhodacosta@gmail.com>
2025-03-06 12:34:39 -0300

    chore: trigger workflow

--- lambda/redirects.js
+++ lambda/redirects.js
@@ -1,7 +1,7 @@
 module.exports = [
   {
     from: "/api/api-timescaledb/",
-    to: "https://docs.timescale.com/api/latest/test",
+    to: "https://docs.timescale.com/api/latest/",
   },
   {
     from: "/api/data-retention/",
